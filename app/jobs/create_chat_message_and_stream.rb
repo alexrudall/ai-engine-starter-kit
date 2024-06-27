0c646a326a4fb9911a50c2a2cf922d514b215e6f
Alex Rudall <alexrudall@gmail.com>
2024-06-27 12:28:23 +0100

    Add comments

--- app/jobs/create_chat_message_and_stream.rb
+++ app/jobs/create_chat_message_and_stream.rb
@@ -2,12 +2,16 @@ class CreateChatMessageAndStream < SidekiqJob
   def perform(args)
     chat_id, user_id, content, model = args.values_at("chat_id", "user_id", "content", "model")
 
+    # Find the user.
     user = User.find(user_id)
 
+    # Find the chat.
     chat = user.chats.find(chat_id)
 
+    # Create the new user message.
     chat.messages.create(content: content, role: "user")
 
+    # Get the response from OpenAI.
     chat.run(model: model)
   end
 end
